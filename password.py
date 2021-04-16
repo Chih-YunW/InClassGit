0b028761b2f17f662b8334e52e3aa56644b0d384
Chih Yun <wuchihy@oregonstate.edu>
2021-04-16 14:11:22 -0700

    create while loop and genarate random int

--- password.py
+++ password.py
@@ -1,1 +1,11 @@
+import random
+
+def password (n):
+	i = 1
+	while i <= n:
+		a = random.randint(32,126)
+
 length = input("Please enter a length for the password: ")
+length = int(length)
+password(length)
+print("\n")
